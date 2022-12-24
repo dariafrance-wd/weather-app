a0bfff723114e3406d43c3c4b2f5ae5bf23ec396
dariafrance-wd <117106505+dariafrance-wd@users.noreply.github.com>
2022-12-24 17:32:12 +0200

    add descrioption

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function dispayWeather(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   let iconElement = document.querySelector("#icon");
   document
     .querySelector("#icon")
